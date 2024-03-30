fc62435e0f0f713b544e1b6b28cf9c8c7c24a9ae
Maricarmen <maricarmencoaquiracuevas@gmail.com>
2024-03-30 12:59:55 -0500

    Structure

---
+++ osha-frontend/src/components/Top.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import '../styles/Header.css'
+
+function Top(){
+  return (
+    <section className="">
+      <div className="">
+        <div>
+          <ul>
+            <li>
+              <a href="">
+                <span></span>
+                <span></span>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span></span>
+                <span></span>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <span></span>
+                <span>info@osha.es</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
+export default Top
